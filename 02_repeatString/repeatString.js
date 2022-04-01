34bb5cf82afa3cc0b90f2e3cace12f0ab10e72ad
lompdoctor <pleclair@protonmail.com>
2022-03-31 21:22:27 -0500

    Solve exercises 1, 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let result = '';
+    if (num < 0)
+        return 'ERROR';
+    for (let i = 0; i < num; i++) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
