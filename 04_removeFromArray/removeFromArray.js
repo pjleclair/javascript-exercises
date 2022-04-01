0cc44b4e5495a92df2cb7e2e0d186166c09272c0
lompdoctor <pleclair@protonmail.com>
2022-04-01 15:03:36 -0500

    Complete exercises 3, 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...vals) {
+    let filter=vals;
+    for (let i = 0; i < filter.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === filter[i]) {
+                array.splice(j,1);
+            }
+        }
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
