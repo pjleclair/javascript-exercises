70d8dd240be356b0319572a4c6056147f612a2f6
lompdoctor <pleclair@protonmail.com>
2022-04-01 16:06:51 -0500

    Complete exercises 5, 6, and 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    /*initial definitions*/
+    let sum = 0;
+    /*check to see if larger number first*/
+    if ((start < 0) || (end < 0)) {
+        return('ERROR');
+    } else if ((Number.isInteger(start)==false) || (Number.isInteger(end) == false)) {
+        return ('ERROR');
+    }
+    if (start > end) {
+        /*loop and sum numbers between end points*/
+        for (start; start >= end; start--) {
+            sum += start;
+        }
+    } else {
+        /*loop and sum numbers between end points*/
+        for (start; start <= end; start++) {
+            sum += start;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newtemp = (temp-32)*(5/9);
+  console.log(newtemp);
+  if ((Number.isInteger(newtemp))==false)  
+    return Number(newtemp.toFixed(1));
+  else
+    return Number(newtemp);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newtemp = (temp*(9/5))+32;
+  console.log(newtemp);
+  console.log(Number.isInteger(newtemp));
+  if ((Number.isInteger(newtemp))==false) {
+    console.log(newtemp.toFixed(1)); 
+    return Number(newtemp.toFixed(1));
+  } else {
+    return Number(newtemp);
+  }
 };
 
 // Do not edit below this line
